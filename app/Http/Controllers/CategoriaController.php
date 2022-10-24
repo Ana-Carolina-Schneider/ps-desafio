29e352baa37f5cea8c5d8a179c179b8c635947e0
Ana-Carolina-Schneider <anaschneider181952@gmail.com>
2022-10-23 21:17:09 -0300

    Implementei os controllers de categoria e produto

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,38 +2,83 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCategoriaRequest;
 use Illuminate\Http\Request;
 
 use App\Models\Categoria;
 
 class CategoriaController extends Controller
 {
+    private $categorias;
 
-
-
-    public function index()
+    public function __construct(Categoria $categoria)
     {
+        $this->categorias = $categoria;
+    }
+
+
+    public function index(CategoriaRequest $request)
+    {
+        $categorias = $this->categorias->all();
+        return view('admin.categoria.index', compact('categorias'));
     }
 
 
     public function create()
     {
+        return view('admin.categoria.crud');
     }
 
 
-    public function store(Request $request)
+    public function store(StoreCategoriaRequest $request)
     {
-    }
+        $datas = $request->all();
+        $categoria = $this->categorias->create($datas);
 
+        //Log de ações
+        $logMessage = 'O usuário ' . '[' . auth()->id() . '] - ' . auth()->user()->name . ' cadastrou uma nova categoria ' . '[' . $categoria->id . '] - ' . $categoria->categoria;
+        Log::create(['log' => $logMessage, 'category' => 'Categorias']);
+
+        return redirect(route('categoria.index'))->with('success', 'Categoria cadastrada com sucesso!');
+    }
+    public function show(CategoriaRequest $request, $id)
+    {
+        $categoria = $this->categorias->find($id);
+
+        return json_encode($categoria);
+    }
     public function edit($id)
     {
+        $categoria = $this->categorias->find($id);
+        return view('admin.categoria.crud', compact('categoria'));
     }
 
     public function update(Request $request, $id)
     {
+        $datas = $request->all();
+        $categoria = $this->categorias->find($id);
+
+        $categoria->update($datas);
+
+        //Log de ações
+        $logMessage = 'O usuário ' . '[' . auth()->id() . '] - ' . auth()->user()->name . ' alterou a categoria ' . '[' . $categoria->id . '] - ' . $categoria->name;
+        Log::create(['log' => $logMessage, 'category' => 'Categoria']);
+
+        return redirect(route('categoria.index'))->with('success', 'Categoria alterada com sucesso!');
     }
 
     public function destroy($id)
     {
+        $categoria = $this->categorias->find($id);
+
+        if ($categoria->id > 1) {
+            $categoria->delete();
+
+            //Log de ações
+            $logMessage = 'O usuário ' . '[' . auth()->id() . '] - ' . auth()->user()->name . ' excluiu a categoria ' . '[' . $categoria->id . '] - ' . $categoria->name;
+            Log::create(['log' => $logMessage, 'category' => 'Categoria']);
+        }
+
+        return redirect(route('categoria.index'))->with('success', 'Categoria excluída com sucesso!');
     }
 }
--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -10,37 +10,80 @@ use Illuminate\Support\Facades\Storage;
 
 class ProdutoController extends Controller
 {
+    private $produtos;
 
-    public function index()
+    public function __construct(Produto $produto)
     {
+        $this->produtos = $produto;
+    }
+
+    public function index(ProdutoRequest $request)
+    {
+        $produtos = $this->produtos->all();
+        return view('admin.produto.index', compact('produtos'));
     }
 
 
     public function create()
     {
+        return view('admin.produto.crud');
     }
 
 
-    public function store(Request $request)
+    public function store(ProdutoRequest $request)
     {
+        $datas = $request->all();
+        $produto = $this->produtos->create($datas);
+
+        //Log de ações
+        $logMessage = 'O usuário ' . '[' . auth()->id() . '] - ' . auth()->user()->name . ' cadastrou um novo produto ' . '[' . $produto->id . '] - ' . $produto->produto;
+        Log::create(['log' => $logMessage, 'category' => 'Produtos']);
+
+        return redirect(route('produto.index'))->with('success', 'Produto cadastrado com sucesso!');
     }
 
 
     public function show($id)
     {
+        $produto = $this->produtos->find($id);
+
+        return json_encode($produto);
     }
 
 
     public function edit($id)
     {
+        $produto = $this->produtos->find($id);
+        return view('admin.produto.crud', compact('produto'));
     }
 
 
     public function update(Request $request, $id)
     {
+        $datas = $request->all();
+        $produto = $this->produtos->find($id);
+
+        $produto->update($datas);
+
+        //Log de ações
+        $logMessage = 'O usuário ' . '[' . auth()->id() . '] - ' . auth()->user()->name . ' alterou o produto ' . '[' . $produto->id . '] - ' . $produto->name;
+        Log::create(['log' => $logMessage, 'category' => 'Categoria']);
+
+        return redirect(route('produto.index'))->with('success', 'Produto alterado com sucesso!');
     }
 
     public function destroy($id)
     {
+        $produto = $this->produtos->find($id);
+
+        if ($produto->id > 1) {
+            $produto->delete();
+
+            //Log de ações
+            $logMessage = 'O usuário ' . '[' . auth()->id() . '] - ' . auth()->user()->name . ' excluiu o produto' . '[' . $produto->id . '] - ' . $produto->name;
+            Log::create(['log' => $logMessage, 'category' => 'Produto']);
+        }
+
+        return redirect(route('produto.index'))->with('success', 'Produto excluído com sucesso!');
     }
 }
